0eb051e0e302347aac88ea9efe6fae8df6e197bc
Salavat <s.faskhutdinov@inbox.ru>
2022-08-19 14:32:13 +0300

    Add new task

--- home_work_1/Program.cs
+++ home_work_1/Program.cs
@@ -48,3 +48,19 @@ Console.Clear();
 
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
+Console.WriteLine("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int M = 1;
+
+int i = M;
+
+while (i < N)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write( i + " " );
+        
+    }
+     i++;
+}
