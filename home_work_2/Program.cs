14967ded34c7d04b2966112c102b817ab9538231
Salavat <s.faskhutdinov@inbox.ru>
2022-08-22 16:17:21 +0300

    add new changes

--- home_work_2/Program.cs
+++ home_work_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(" Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int num1 = number % 100;
+
+Console.WriteLine(num1);
+
+int num2 = num1 / 10;
+ 
+Console.WriteLine(num2);
+
+
+
+
