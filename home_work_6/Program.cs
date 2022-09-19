f3f3fd0e19d2d84e86cc8dd0d059232eba6b3d55
Salavat <s.faskhutdinov@inbox.ru>
2022-09-19 18:02:34 +0300

    add new change 44

---
+++ home_work_6/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.Clear();
+
+
+/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Укажите количество вводимых чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+int[] array(int number)
+{
+    int[] result = new int[number];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число: ");
+        result[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return result;
+}
+
+void showArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void showSum(int[] newArray)
+{
+    int sum = 0;
+
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i] > 0) sum += 1;
+    }
+    Console.WriteLine($"Количество чисел больше 0 = {sum}");
+}
+
+
+
+
+int[] test = array(num);
+showArray(test);
+Console.WriteLine();
+showSum(test);*/
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+/*
